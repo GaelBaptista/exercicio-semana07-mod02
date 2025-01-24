0144e038da947eac9e55027d7254a326c0599f05
bruno_costa_fig <brunocosta.fig@gmail.com>
2025-01-24 19:36:11 -0300

    base do projeto

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from "./data-source"
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+AppDataSource.initialize().then(async () => {
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta http://localhost:3000")
+    })
+
+}).catch(error => console.log(error))
